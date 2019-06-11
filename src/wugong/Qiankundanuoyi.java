0a4f03e0e74a84ccd4cb34939a5958e5ee856c26
wenxitong <473080173@qq.com>
2019-06-11 22:41:54 +0800

    创建

---
+++ src/wugong/Qiankundanuoyi.java
@@ -0,0 +1,14 @@
+package wugong;
+
+public class Qiankundanuoyi {
+
+	public Qiankundanuoyi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
